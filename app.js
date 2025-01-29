3b5dd53c8ee3ed00c4af9d5a4ea130b8d3b0cf0e
Mesi14 <Mesi18@proton.me>
2025-01-29 09:22:42 +0200

    :construction: add getWeatherCity function to js with :bug:

---
+++ app.js
@@ -0,0 +1,15 @@
+const api_key = '';
+const city = document.getElementById("city-name");
+const searchBtn = document.getElementById("search");
+
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(city.value);
+});
+
+let url = `https://api.api-ninjas.com/v1/weather?city=${city.value}`
+const getWetherData = city => {
+    try {
+        const response = await fetch(url)
+    }    
+})
