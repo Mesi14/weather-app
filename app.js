c32a646896879f0532f75a96c890405e7174db5c
Mesi14 <Mesi18@proton.me>
2025-02-01 17:22:15 +0200

    :sparkles: add API fetch and display functionality within app.js

--- app.js
+++ app.js
@@ -1,75 +1,82 @@
-const api_key = '';
+let api_key = "";
+let urlDataRoot = "http://api.openweathermap.org/geo/1.0/direct?q=";
+let urlRoot = "https://api.openweathermap.org/data/3.0/onecall?lat=";
+const pageBody = document.querySelector('body');
 const city = document.getElementById("city-name");
 const searchBtn = document.getElementById("search");
 const info = document.querySelector(".weather");
 const current_temp = document.querySelector(".temp");
 const current_city = document.querySelector(".city");
-const minTemp = document.querySelector(".min_temp");
-const maxTemp = document.querySelector(".max_temp");
+const feels_like = document.querySelector(".feels_like_temp");
+const pressure = document.querySelector(".pressure");
 const windSpeed = document.querySelector(".wind")
 const humidity = document.querySelector(".humidity");
+const current_image = document.querySelector(".icon")
+
+
+const loadConfig = async () => {
+    try {
+        const resp = await fetch("config/config.json");
+        const data = await resp.json();
+        api_key = data.api_key
+    } catch (error) {
+        console.log("Config error", error);
+    }
+}
+
+loadConfig();
 
 searchBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    getWetherData(city.value);
+    current_city.innerHTML = city.value;
+    getCityData(city.value);
 });
 
 const showData = data => {
-    current_temp.innerHTML = data.temp + "°C";
-    minTemp.innerHTML = data.min_temp + "°C";
-    maxTemp.innerHTML = data.max_temp + "°C";
-    humidity.innerHTML = data.humidity + "%";
-    windSpeed.innerHTML = data.wind_speed + "km/h";
-
+    let icon = data.current.weather[0].icon;
+    let current_icon = `https://openweathermap.org/img/wn/${icon}@2x.png`
+    if(data.current.weather[0].main === 'Clear') {
+        document.body.style.backgroundImage = "url('./Assets/Images/sunny.jpeg')";
+    } else if(data.current.weather[0].main === 'Rain') {
+        document.body.style.backgroundImage = "url('./Assets/Images/rainy.jpg')";
+    } else if(data.current.weather[0].main === 'Snow') {
+        document.body.style.backgroundImage = "url('./Assets/Images/snowfall.jpg')";
+    } else if(data.current.weather[0].main === 'Clouds') {
+        document.body.style.backgroundImage = "url('./Assets/Images/cloudy.jpg')";
+    }
+    current_temp.innerHTML = data.current.temp + "°C";
+    feels_like.innerHTML = data.current.feels_like + "°C";
+    current_image.src = current_icon;
+    pressure.innerHTML = data.current.pressure + "hPa";
+    humidity.innerHTML = data.current.humidity + "%";
+    windSpeed.innerHTML = data.current.wind_speed + "km/h";
     info.style.visibility = "visible";
 }
 
-
-// cloud_pct
-// : 
-// 75
-// feels_like
-// : 
-// 4
-// humidity
-// : 
-// 76
-// max_temp
-// : 
-// 8
-// min_temp
-// : 
-// 7
-// sunrise
-// : 
-// 1738135573
-// sunset
-// : 
-// 1738167645
-
-// wind_degrees
-// : 
-// 240
-// wind_speed
-// : 
-// 7.2
-
-const getWetherData = async city => {
-    let url = `https://api.api-ninjas.com/v1/weather?city=${city}`
+const getCityData = async cityName => {
+    let url = `${urlDataRoot}${cityName}&limit=${2}&appid=${api_key}`
     try {
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                "X-Api-Key": api_key
-            }
-        },)
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log("Successful response", result);
+        let currLat = Math.round(result[0].lat * 100) / 100;
+        let currLon = Math.round(result[0].lon * 100) / 100;
+        getWeatherData( currLat, currLon);
+    } catch (err) {
+        console.log("Data error", err);
+    }
+}
+
+const getWeatherData = async (lat, lon) => {
+    console.log(lat, lon);
+    let url = `${urlRoot}${lat}&lon=${lon}&units=metric&appid=${api_key}`;
+    try {
+        const response = await fetch(url)
         const res = await response.json();
         console.log("Success", res)
-        city.innerHTML = city.value
         showData(res);
-
     }    
     catch(err) {
-        console.log("Sorry", err);
+        console.log("The current error: ",  err);
     };
 };
